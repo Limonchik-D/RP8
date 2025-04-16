b3368ec9001b522eb001609e4e2aa7d7fe6ebe81
Limonchik-D <DavLimon4@yandex.com>
2025-04-16 21:39:08 +0300

    Add files via upload

---
+++ Task 2 Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int D = 3;
+        List<int> A = new List<int> { 13, 3, -13, 23, 33, 13, 43, 23, 53, 63 };
+
+ 
+        var filtered = A
+            .Where(n => n > 0 && n % 10 == D) // окончание на D и положительные
+            .Reverse()                        // переворачиваем список
+            .Distinct()                       // убираем повторы, оставляя последние вхождения
+            .Reverse();                       // возвращаем исходный порядок
+
+        Console.WriteLine("Результат:");
+        foreach (var num in filtered)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
