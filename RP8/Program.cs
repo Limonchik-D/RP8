fb89cab378a86491e60e83774dacc277a9935768
Limonchik-D <DavLimon4@yandex.com>
2025-04-16 21:37:50 +0300

    Add files via upload

---
+++ RP8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        char c = 'a';
+        List<string> A = new List<string> { "ab", "aa", "abc", "a", "ba", "aaa", "cbc", "aca" };
+
+        int count = A.Count(s => s.Length > 1 && s.StartsWith(c.ToString()) && s.EndsWith(c.ToString()));
+
+        Console.WriteLine($"Количество подходящих строк: {count}");
+    }
+}
