fe5163601c3e8269146c469c5a449350edb30ae9
Limonchik-D <DavLimon4@yandex.com>
2025-04-16 21:40:23 +0300

    Add files via upload

---
+++ Task 4 Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> A = new List<int> { 49, 12, 35, 57 };
+        List<int> B = new List<int> { 129, 7, 15, 92, 67 };
+
+        var result = A
+            .SelectMany(a => B
+                .Where(b => a % 10 == b % 10) // сравниваем последние цифры
+                .Select(b => $"{a}-{b}")     // создаём строку "a-b"
+            )
+            .ToList();
+
+        Console.WriteLine("Результат:");
+        foreach (var pair in result)
+        {
+            Console.WriteLine(pair);
+        }
+    }
+}
